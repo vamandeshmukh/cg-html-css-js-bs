3ce358b51b16da4f3a317e15eca7d11dbe31c231
vamandeshmukh <vamandeshmukh@gmail.com>
2022-07-07 11:36:31 +0530

    JavaScript

--- js-demos/01-js-basics.js
+++ js-demos/01-js-basics.js
@@ -1,4 +1,12 @@
 // run this file on CMD with - 
-//  node 01-js-basics.js
+// node 01-js-basics.js
+
+/*
+comment 
+comment 
+*/
 
 console.log("JavaScript Basics");
+console.log('JavaScript Basics');
+console.log(`JavaScript Basics`);
+console.log(10 + 20);
