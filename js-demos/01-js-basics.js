75e4cca02cd31fb9ab2ccb55fa191a066ad07751
vamandeshmukh <vamandeshmukh@gmail.com>
2022-07-07 11:44:04 +0530

    JavaScript

--- js-demos/01-js-basics.js
+++ js-demos/01-js-basics.js
@@ -6,7 +6,40 @@ comment
 comment 
 */
 
-console.log("JavaScript Basics");
-console.log('JavaScript Basics');
-console.log(`JavaScript Basics`);
-console.log(10 + 20);
+// console.log("JavaScript Basics");
+// console.log('JavaScript Basics');
+// console.log(`JavaScript Basics`);
+// console.log(10 + 20);
+
+// basics of JS 
+
+// primitive types in JS -
+// number, string, boolean 
+
+// Java -  int num4 = 10;
+
+num = 10; // do not use this 
+var num2 = 20; // do not use this 
+let num3 = 30; // use this for variables whose value may change  
+const num4 = 40; // use this for final variables 
+
+console.log(num);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
