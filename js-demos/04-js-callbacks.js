0d83390d65414efd161ddd88a299d2be238e8d52
vamandeshmukh <vamandeshmukh@gmail.com>
2022-07-07 17:19:16 +0530

    JavaScript

--- js-demos/04-js-callbacks.js
+++ js-demos/04-js-callbacks.js
@@ -2,14 +2,76 @@
 
 // callbacks in JS 
 
-console.log("one");
+// console.log("one");
 
-// setTimeout(arg1, arg2);
-// setTimeout(() => {}, ms);
-// setTimeout(() => {}, 2000);
+// // setTimeout(arg1, arg2);
+// // setTimeout(() => {}, ms);
+// // setTimeout(() => {}, 2000);
 
-setTimeout(() => {
-    console.log("two");
-}, 2000);
+// setTimeout(() => {
+//     console.log("two");
+// }, 2000);
+
+
+// console.log("one");
+
+// let num = 10;
+
+// setTimeout(() => {
+//     num = 20;
+// }, 2000);
+
+// console.log(num);
+
+// // problems of asynchronous JS 
+
+// console.log('begin...'); // 1
+
+// const getFun = () => {
+//     setTimeout(() => {
+//         return { messsage: 'Have fun!' }; // 2
+//     }, 2000);
+// }
+
+// const fun = getFun(); // 3
+// console.log(fun);  // 4
+
+// 1. solution with callback 
+// callback - pass a function as argument to another function  
+
+console.log('begin...'); // 1
+
+const getFun = (callBack) => {
+    setTimeout(() => {
+        callBack({ messsage: 'Have fun!' }); // 2
+    }, 2000);
+}
+
+// getFun(() => {});
+
+getFun((fun) => {
+    console.log(fun.messsage);  // 4
+}); // 3
+
+// // 1. same solution with callback - separately
+
+// console.log('begin...'); // 1
+
+// const getFun = (callBack) => {
+//     setTimeout(() => {
+//         callBack({ messsage: 'Have fun!' }); // 2
+//     }, 2000);
+// }
+
+// const callbackFun = (fun) => {
+//     console.log(fun.messsage);
+// }
+
+// getFun(callbackFun); // 3
+
+
+
+
+
 
 
