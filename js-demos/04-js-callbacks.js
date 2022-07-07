7485f29870a6135535004b3657f4b895a14bee7e
vamandeshmukh <vamandeshmukh@gmail.com>
2022-07-07 17:22:25 +0530

    JavaScript

--- js-demos/04-js-callbacks.js
+++ js-demos/04-js-callbacks.js
@@ -1,6 +1,8 @@
 
 
+// ---------------
 // callbacks in JS 
+// ---------------
 
 // console.log("one");
 
@@ -12,6 +14,9 @@
 //     console.log("two");
 // }, 2000);
 
+// ---------------
+// callbacks in JS 
+// ---------------
 
 // console.log("one");
 
@@ -23,37 +28,43 @@
 
 // console.log(num);
 
-// // problems of asynchronous JS 
-
-// console.log('begin...'); // 1
-
-// const getFun = () => {
-//     setTimeout(() => {
-//         return { messsage: 'Have fun!' }; // 2
-//     }, 2000);
-// }
-
-// const fun = getFun(); // 3
-// console.log(fun);  // 4
-
-// 1. solution with callback 
-// callback - pass a function as argument to another function  
+// ----------------------------
+// problems of asynchronous JS 
+// ----------------------------
 
 console.log('begin...'); // 1
 
-const getFun = (callBack) => {
+const getFun = () => {
     setTimeout(() => {
-        callBack({ messsage: 'Have fun!' }); // 2
+        return { messsage: 'Have fun!' }; // 2
     }, 2000);
 }
 
+const fun = getFun(); // 3
+console.log(fun);  // 4
+
+// -------------------------------------------------------------
+// // 1. solution with callback 
+// // callback - pass a function as argument to another function  
+// -------------------------------------------------------------
+
+// console.log('begin...'); // 1
+
+// const getFun = (callBack) => {
+//     setTimeout(() => {
+//         callBack({ messsage: 'Have fun!' }); // 2
+//     }, 2000);
+// }
+
 // getFun(() => {});
 
-getFun((fun) => {
-    console.log(fun.messsage);  // 4
-}); // 3
+// getFun((fun) => {
+//     console.log(fun.messsage);  // 4
+// }); // 3
 
+// ----------------------------------------------
 // // 1. same solution with callback - separately
+// ----------------------------------------------
 
 // console.log('begin...'); // 1
 
