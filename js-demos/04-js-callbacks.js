9019a0ff815ec4c7042a1dfc8db80191dd7155e6
vamandeshmukh <vamandeshmukh@gmail.com>
2022-07-07 16:42:34 +0530

    JavaScript

---
+++ js-demos/04-js-callbacks.js
@@ -0,0 +1,15 @@
+
+
+// callbacks in JS 
+
+console.log("one");
+
+// setTimeout(arg1, arg2);
+// setTimeout(() => {}, ms);
+// setTimeout(() => {}, 2000);
+
+setTimeout(() => {
+    console.log("two");
+}, 2000);
+
+
