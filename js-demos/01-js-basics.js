c6c4811e6ceb8694ff8ec2a8ba24f4a4b2e81733
vamandeshmukh <vamandeshmukh@gmail.com>
2022-07-07 12:15:14 +0530

    JavaScript

--- js-demos/01-js-basics.js
+++ js-demos/01-js-basics.js
@@ -19,24 +19,51 @@ comment
 // Java -  int num4 = 10;
 // Java -  double num5 = 10.5;
 
-num = 10; // do not use this 
-var num2 = 20; // do not use this
+// num = 10; // do not use this 
+// var num2 = 20; // do not use this
 
-let num3 = 30; // use this for variables whose value may change  
-const num4 = 40; // use this for final variables 
+// let num3 = 30; // use this for variables whose value may change  
+// const num4 = 40; // use this for final variables 
+
+// console.log(num);
+// console.log(num2);
+// console.log(num3);
+// console.log(num4);
+
+// let someVariableName = 10;
+// let someVariableName2 = "Vaman";
+// let someVariableName3 = false;
+
+// console.log(someVariableName);
+// console.log(someVariableName2);
+// console.log(someVariableName3);
+
+// diff b/w let and const 
+
+// let num = 10;
+// const num2 = 20;
+
+// console.log(num); // 10 
+// console.log(num2); // 20 
+// num = 15;
+// console.log(num); // 15 
+// num2 = 25; // ?
+// console.log(num2); // ?
+
+let num = 10;
+let num2 = "abc";
+let num3 = false;
+let num4;
 
 console.log(num);
+console.log(typeof (num));
 console.log(num2);
+console.log(typeof (num2));
 console.log(num3);
+console.log(typeof (num3));
 console.log(num4);
+console.log(typeof (num4));
 
-let someVariableName = 10;
-let someVariableName2 = "Vaman";
-let someVariableName3 = false;
-
-console.log(someVariableName);
-console.log(someVariableName2);
-console.log(someVariableName3);
 
 
 
